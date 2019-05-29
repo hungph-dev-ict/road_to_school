5324c7e08eee60ddc269e1302331d1dd098a7985
hungph_dev_ict <hungph.dev.ict@gmail.com>
2019-05-29 21:39:13 +0700

    Fix assets directory

---
+++ webpack.mix.js
@@ -0,0 +1,16 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+// mix.copyDirectory('resources/assets/RoadToSchool-css', 'public/assets');
+// mix.copyDirectory('resources/assets/RoadToSchool-js', 'public/assets');
+mix.copyDirectory('resources/assets/r2s_assets', 'public/assets');
